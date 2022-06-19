7b619aa5dcfaf28de0a51476a4fb6b7b8428739b
Konstantin <karfogent3@gmail.com>
2022-06-19 23:24:44 +0300

    change scroll

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 const opacityOfNav = document.querySelector("nav")
-
+scrollPrev = 0;
 window.addEventListener("scroll", function () {
-    if (scrollY > 10) {
+    if (scrollY > 10 && scrollY > scrollPrev) {
         opacityOfNav.classList.add("NoOpacity")
         opacityOfNav.classList.remove("YeOpacity")
     } else {
         opacityOfNav.classList.remove("NoOpacity")
         opacityOfNav.classList.add("YeOpacity")
     }
+    scrollPrev = this.scrollY
 })
